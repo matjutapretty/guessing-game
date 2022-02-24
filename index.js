32261af8bb5ccb6894c54a17246050a56b90ce08
Pretty <mphefopretty1997@gmail.com>
2022-02-24 22:32:41 +0200

    made changes according to the feedback from mentors

--- index.js
+++ index.js
@@ -4,22 +4,48 @@ const numberGuess = document.querySelector(".numberGuess");
 const guessBtn = document.querySelector(".guessBtn");
 const message = document.querySelector(".message");
 
+function newGame() {
+    setTimeout(function() {
+        message.innerHTML = ""
+    }, 3000)
+}
 
 function guessBtnClicked() {
 	const guessedNo = Number(numberGuess.value);
 
-	if (guessedNo < randomNumber) {
+	if (guessedNo > 100 || guessedNo < 1) {
+        message.innerHTML = 'You have exceeded the limit, enter a number between 1 & 100';
+
+        setTimeout(function() {
+            location.reload()
+        }, 3000);
+
+	}else if (guessedNo < randomNumber) {
 		message.innerHTML = "Your guess is too low!";
+		setTimeout(function(){
+			message.innerHTML = "";
+		}, 3000);
+
 	} else if (guessedNo > randomNumber) {
 		message.innerHTML = "Your guess is too high!";
+		setTimeout(function(){
+			message.innerHTML = "";
+		}, 3000);
+
 	} else {
 		message.innerHTML =  `Correct, the secret number is ${guessedNo}!`;
-		reset();
-	}
-	setTimeout(function(){
+		reset()
+		setTimeout(function(){
 		message.innerHTML = "";
-	}, 2500);
+		newGame()
+		message.innerHTML = 'New Game Started!!!'
+	}, 3000);
 }
+}
+	newGame()
+	message.innerHTML = 'New Game Started!!!'
+
+
 function reset() {
 	randomNumber = Math.ceil((Math.random() * 100))
 }
